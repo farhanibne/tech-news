4016ec1635c619194f0e94d34b7958709a23d336
farhan ibne saif <84982846+farhanibne@users.noreply.github.com>
2022-02-15 16:20:53 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchForm from './SearchForm'
+import Stories from './Stories'
+import Buttons from './Buttons'
+function App() {
+  return (
+    <div  >
+      <SearchForm />
+      <Stories />
+      <Buttons />
+    </div>
+  )
+}
+
+export default App
